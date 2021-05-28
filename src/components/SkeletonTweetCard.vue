bfb106e5746fa79746fec2acf6ed01d4f0b5f2e1
xiao0232 <t409828543087@gmail.com>
2021-05-28 22:46:58 +0900

    add: skeleton-loading component

---
+++ src/components/SkeletonTweetCard.vue
@@ -0,0 +1,119 @@
+<template>
+  <div v-if="isLoading" class="skeleton-screen" >
+  <!-- <div class="skeleton-screen" > -->
+    <div class="skeleton skeleton__circle" ></div>
+    <div class="skeleton skeleton__screen-name" ></div>
+    <div class="skeleton skeleton__screen-id" ></div>
+    <div class="skeleton__text__container" >
+      <div class="skeleton skeleton__text" ></div>
+      <div class="skeleton skeleton__text" ></div>
+      <div class="skeleton skeleton__text" ></div>
+      <div class="skeleton skeleton__text" ></div>
+    </div>
+    <div class="skeleton skeleton__time" ></div>
+    <div class="skeleton skeleton__hr" ></div>
+    <div class="skeleton skeleton__footer" ></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "SkeletonTweetCard",
+  props: {
+    isLoading: {
+      type: Boolean,
+      defoult: false
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.skeleton-screen {
+  display: flex;
+  max-width: 550px;
+  min-width: 250px;
+  width: 100%;
+  height: 300px;
+  margin: 10px auto;
+  border-radius: 12px;
+  background-color: #a7a7a7;
+  position: relative;
+  overflow: hidden;
+}
+
+.skeleton {
+  margin: 10px auto;
+  background-color: #d9d9d9;
+  position: absolute;
+  overflow: hidden;
+  
+  &__circle {
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    top: 12px;
+    left: 16px;
+  }
+  &__screen-name {
+    width: 140px;
+    height: 14px;
+    top: 18px;
+    left: 68px;
+  }
+  &__screen-id {
+    width: 100px;
+    height: 14px;
+    top: 38px;
+    left: 68px;
+  }
+  @for $i from 1 through 4 {
+    &__text:nth-of-type(#{$i}) {
+      width: 515px;
+      height: 14px;
+      top: calc(80px + (#{$i} - 1) * 24px);
+      left: 16px;
+    }
+  }
+  &__time {
+    width: 160px;
+    height: 20px;
+    top: 200px;
+    left: 16px;
+  }
+  &__hr {
+    max-width: 515px;
+    width: calc(100% - 2 * 16px);
+    height: 2px;
+    top: 230px;
+    left: 16px;
+  }
+  &__footer {
+    max-width: 515px;
+    width: calc(100% - 2 * 16px);
+    height: 32px;
+    top: 240px;
+    left: 16px;
+  }
+
+  &:before {
+    content: '';
+    display: block;
+    height: 100%;
+    width: 100%;
+    background: linear-gradient(to right, transparent, rgba(255, 255, 255, .5), transparent);
+    animation: skeleton-animation 1s linear infinite;
+  }
+}
+  
+@keyframes skeleton-animation {
+  0% {
+    transform: translateX(-100%)
+  }
+  100% {
+    transform: translateX(100%)
+  }
+}
+</style>
--- src/components/TweetCard.vue
+++ src/components/TweetCard.vue
@@ -1,27 +1,19 @@
 <template>
   <div class="tweet-card">
-    <div v-if="isLoading" class="skeleton-screen" >
-    <!-- <div class="skeleton-screen" > -->
-      <div class="skeleton-circle" ></div>
-      <div class="skeleton-screen-name" ></div>
-      <div class="skeleton-screen-id" ></div>
-      <div class="skeleton-text-1" ></div>
-      <div class="skeleton-text-2" ></div>
-      <div class="skeleton-text-3" ></div>
-      <div class="skeleton-text-4" ></div>
-      <div class="skeleton-time" ></div>
-      <div class="skeleton-hr" ></div>
-      <div class="skeleton-footer" ></div>
-    </div>
-    <div ref="tweet" class="tweet-card" ></div>
+    <SkeletonTweetCard :isLoading="isLoading" />
+    <div ref="tweet" class="tweet-card"></div>
   </div>
 </template>
 
 <script lang="ts">
 import { ref, defineComponent, onMounted, nextTick, watch } from "vue"
+import SkeletonTweetCard from "./SkeletonTweetCard.vue"
 
 export default defineComponent({
   name: "TweetCard",
+  components: {
+    SkeletonTweetCard
+  },
   props: {
     tweet_id: {
       type: String,
@@ -45,7 +37,7 @@ export default defineComponent({
         return
       }
       
-      isLoading.value = true;
+      isLoading.value = true
       
       await (<any>window).twttr
         .ready()
@@ -57,10 +49,9 @@ export default defineComponent({
             .createTweet(props.tweet_id, tweet.value)
             .then(() => {
               nextTick()
-              isLoading.value = false
             })
             .finally(() => {
-              isLoading.value = false;
+              isLoading.value = false
             })
         })
         
@@ -78,215 +69,12 @@ export default defineComponent({
 })
 </script>
 
-<style>
+<style lang="scss">
 .tweet-card{
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-}
-
-.skeleton-screen {
-  display: block;
-  width: 550px;
-  height: 300px;
-  margin: 10px auto;
-  border-radius: 12px;
-  background-color: #a7a7a7;
-  position: relative;
-  overflow: hidden
-}
-
-.skeleton-circle {
-  width: 48px;
-  height: 48px;
-  margin: 10px auto;
-  border-radius: 50%;
-  background-color: #d9d9d9;
-  position: absolute;
-  top: 12px;
-  left: 16px;
-  overflow: hidden
-}
-.skeleton-circle:before {
-  content: '';
-  display: block;
-  height: 100%;
-  width: 100%;
-  background: linear-gradient(to right, transparent, rgba(255, 255, 255, .5), transparent);
-  animation: skeleton-animation 1.2s linear infinite;
-}
-.skeleton-screen-name {
-  width: 140px;
-  height: 14px;
-  margin: 10px auto;
-  background-color: #d9d9d9;
-  position: absolute;
-  top: 18px;
-  left: 68px;
-  overflow: hidden
-}
-.skeleton-screen-name:before {
-  content: '';
-  display: block;
-  height: 100%;
-  width: 100%;
-  background: linear-gradient(to right, transparent, rgba(255, 255, 255, .5), transparent);
-  animation: skeleton-animation 1.2s linear infinite;
-}
-.skeleton-screen-id {
-  width: 100px;
-  height: 14px;
-  margin: 10px auto;
-  background-color: #d9d9d9;
-  position: absolute;
-  top: 38px;
-  left: 68px;
-  overflow: hidden
-}
-.skeleton-screen-id:before {
-  content: '';
-  display: block;
-  height: 100%;
-  width: 100%;
-  background: linear-gradient(to right, transparent, rgba(255, 255, 255, .5), transparent);
-  animation: skeleton-animation 1.2s linear infinite;
-}
-.skeleton-text-1 {
-  width: 515px;
-  height: 14px;
-  margin: 10px auto;
-  background-color: #d9d9d9;
-  position: absolute;
-  top: 80px;
-  left: 16px;
-  overflow: hidden
-}
-.skeleton-text-1:before {
-  content: '';
-  display: block;
-  height: 100%;
-  width: 100%;
-  background: linear-gradient(to right, transparent, rgba(255, 255, 255, .5), transparent);
-  animation: skeleton-animation 1.2s linear infinite;
-}
-.skeleton-text-2 {
-  width: 515px;
-  height: 14px;
-  margin: 10px auto;
-  background-color: #d9d9d9;
-  position: absolute;
-  top: 104px;
-  left: 16px;
-  overflow: hidden
-}
-.skeleton-text-2:before {
-  content: '';
-  display: block;
-  height: 100%;
-  width: 100%;
-  background: linear-gradient(to right, transparent, rgba(255, 255, 255, .5), transparent);
-  animation: skeleton-animation 1.2s linear infinite;
-}
-.skeleton-text-3 {
-  width: 515px;
-  height: 14px;
-  margin: 10px auto;
-  background-color: #d9d9d9;
-  position: absolute;
-  top: 128px;
-  left: 16px;
-  overflow: hidden
-}
-.skeleton-text-3:before {
-  content: '';
-  display: block;
-  height: 100%;
-  width: 100%;
-  background: linear-gradient(to right, transparent, rgba(255, 255, 255, .5), transparent);
-  animation: skeleton-animation 1.2s linear infinite;
-}
-.skeleton-text-4 {
-  width: 515px;
-  height: 14px;
-  margin: 10px auto;
-  background-color: #d9d9d9;
-  position: absolute;
-  top: 152px;
-  left: 16px;
-  overflow: hidden
-}
-.skeleton-text-4:before {
-  content: '';
-  display: block;
-  height: 100%;
-  width: 100%;
-  background: linear-gradient(to right, transparent, rgba(255, 255, 255, .5), transparent);
-  animation: skeleton-animation 1.2s linear infinite;
-}
-.skeleton-time {
-  width: 160px;
-  height: 20px;
-  margin: 10px auto;
-  background-color: #d9d9d9;
-  position: absolute;
-  top: 200px;
-  left: 16px;
-  overflow: hidden
-}
-.skeleton-time:before {
-  content: '';
-  display: block;
-  height: 100%;
-  width: 100%;
-  background: linear-gradient(to right, transparent, rgba(255, 255, 255, .5), transparent);
-  animation: skeleton-animation 1.2s linear infinite;
-}
-.skeleton-hr {
-  width: 515px;
-  height: 2px;
-  margin: 10px auto;
-  background-color: #d9d9d9;
-  position: absolute;
-  top: 230px;
-  left: 16px;
-  overflow: hidden
-}
-.skeleton-hr:before {
-  content: '';
-  display: block;
-  height: 100%;
-  width: 100%;
-  background: linear-gradient(to right, transparent, rgba(255, 255, 255, .5), transparent);
-  animation: skeleton-animation 1.2s linear infinite;
-}
-.skeleton-footer {
-  width: 515px;
-  height: 32px;
-  margin: 10px auto;
-  background-color: #d9d9d9;
-  position: absolute;
-  top: 240px;
-  left: 16px;
-  overflow: hidden
-}
-.skeleton-footer:before {
-  content: '';
-  display: block;
-  height: 100%;
-  width: 100%;
-  background: linear-gradient(to right, transparent, rgba(255, 255, 255, .5), transparent);
-  animation: skeleton-animation 1.2s linear infinite;
-}
-
-  
-@keyframes skeleton-animation {
-  0% {
-    transform: translateX(-100%)
-  }
-  100% {
-    transform: translateX(100%)
-  }
 }
 </style>
